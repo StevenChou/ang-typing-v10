849b3345e67532d5b8c4bcbd85a1726aba3e132f
whalebro <steven.chou.second@gmail.com>
2020-10-12 22:18:19 +0800

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { lorem } from 'faker';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing-v10';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string): void {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string): string {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
